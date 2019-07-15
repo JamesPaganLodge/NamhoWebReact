14159a89bd4bbfbc1fc70a7ba24d1f4800ba0707
James Pagan-Lodge <stixx_60@hotmail.com>
2019-07-16 00:26:42 +0100

    Add pages

---
+++ src/pages/Bookings.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Bookings extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        Bookings
+      </div>
+    )
+  }
+}
+
+export default Bookings
---
+++ src/pages/Trips.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Trips extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        Trips
+      </div>
+    )
+  }
+}
+
+export default Trips
