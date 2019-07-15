032e94dee9096c2cb95df3788b3e478b3c3ddc3b
James Pagan-Lodge <stixx_60@hotmail.com>
2019-07-15 23:47:24 +0100

    initial commit

---
+++ src/components/NamhoHeader.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class NamhoHeader extends Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <header>
+
+      </header>
+    )
+  }
+}
+
+export default NamhoHeader
