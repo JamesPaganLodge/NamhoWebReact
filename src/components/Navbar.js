7f57eb326c8a523af8c78127cbc1e6baddbf93be
James Pagan-Lodge <stixx_60@hotmail.com>
2019-07-16 22:58:30 +0100

    Add routes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+
+import Home from '../pages/Home'
+import Trips from '../pages/Trips'
+import Bookings from '../pages/Bookings'
+import Contact from '../pages/Contact'
 
 class Navbar extends Component {
   constructor() {
@@ -8,9 +14,22 @@ class Navbar extends Component {
 
   render() {
     return (
-      <div>
-        Navbar
-      </div>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li><Link to={'/'}>Home</Link></li>
+            <li><Link to={'/Trips'}>Trips</Link></li>
+            <li><Link to={'/Bookings'}>Bookings</Link></li>
+            <li><Link to={'/Contact'}>Contact</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/Trips' component={Trips} />
+          <Route path='/Bookings' component={Bookings} />
+          <Route path='/Contact' component={Contact} />
+        </Switch>
+      </BrowserRouter>      
     )
   }
 }
