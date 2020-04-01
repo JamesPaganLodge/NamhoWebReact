ba83dd44c153111d86537dc1a33ce2a6f5b05ba9
James Pagan-Lodge <stixx_60@hotmail.com>
2020-04-01 12:06:35 +0100

    Add avatar

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import avatar from '../images/avatar_default.png'
 import '../css/Login.css'
 
 function Login(props) {
   return (
-    <div className="login-container">
-      <p className="login-link">Login</p>
+    <div className="login-container">      
+      {/* <p className="login-link">Login</p> */}
+      <img className="login-avatar" 
+        src={avatar}
+        alt="Login/Sign up" />
     </div>
   )
 }
